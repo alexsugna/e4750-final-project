8f938634afab0a1138f93f5858b475138c9a8784
skylers27 <32472386+skylers27@users.noreply.github.com>
2021-12-03 01:42:17 -0500

    serial implementation working
    squared error and divergence objective functions both work with new york times test data set

---
+++ serial_div.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data_path = 'nyt_data.txt'
+vocab_path = 'nyt_vocab.dat'
+data = pd.read_csv(data_path, sep='\n', header=None)
+vocab = pd.read_csv(vocab_path, sep='\n', header=None, names=['words'])
+
+N = 3012 #number of words
+M = 8447 #number of articles
+X = np.zeros((N, M)) #define the original data matrix
+
+#load data, every row is a single document
+#format “idx:cnt” with commas separating each unique word in the document
+for column in range(M):
+    row = data.iloc[column].values[0].split(',')
+    for item in row:
+        index, count = map(int, item.split(':'))
+        X[index-1][column] = count
+
+err = 1e-16 #a small error to prevent 0/0
+iteration = 100 #number of iterations
+K = 25 #number of categories (matrix rank)
+
+W = np.random.uniform(1, 2, (N, K)) #initialize W and H to random values between 1 and 2
+H = np.random.uniform(1, 2, (K, M))
+
+divergence_out = [] #keep track of objective function for each iteration
+for i in range(1, iteration+1):
+    if i % 10 == 0:
+        print('iteration %d' % i)
+    P = X / (W.dot(H)+err) #intermediate step (purple matrix in notes)
+    Wt = W.T
+    Wt = Wt / Wt.sum(axis=1).reshape(-1, 1) #normalize rows
+    H = H * (Wt.dot(P))  #update H
+    P = X / (W.dot(H)+err)
+    Ht = H.T
+    Ht = Ht / Ht.sum(axis=0).reshape(1, -1) #normalize columns
+    W = W * (P.dot(Ht))  #update W
+    divergence_out.append(np.sum(-X * np.log(W.dot(H)+err) + W.dot(H)))
+
+#plot the objective function, should monotonically decrease to a local minimum
+fig = plt.figure()
+plt.plot(np.arange(1, 101), divergence_out, label='divergence objective function')
+plt.grid()
+plt.legend()
+plt.ylabel('value')
+plt.xlabel('iteration')
+plt.title('Divergence NMF')
+plt.savefig('test_divergence.png')
+
+W = W / W.sum(axis=0).reshape(1,-1) #normalize the 25 categories
+
+#find the 10 most used words for each category and print
+data = pd.DataFrame(index=range(10), columns=['Topic_%d' % i for i in range(1, 26)])
+for i in range(25):
+    column = 'Topic_' + str(i+1)
+    Wi = W[:, i]
+    dt = pd.DataFrame(Wi, columns=['weight'])
+    dt['words'] = vocab
+    dt = dt.sort_values(by='weight', ascending=False)[:10].reset_index(drop=True)
+    data[column] = dt['weight'].map(lambda x: ('%.4f')%x) + ' ' + dt['words']
+print(data)
