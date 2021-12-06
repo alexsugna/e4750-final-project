3f759a921e51116ad52f4aea130866260057ffb2
Alex Angus <ala2197@columbia.edu>
2021-12-06 20:19:13 +0000

    define index variable in kernel

--- kernels/.ipynb_checkpoints/ElementWise-checkpoint.cu
+++ kernels/.ipynb_checkpoints/ElementWise-checkpoint.cu
@@ -1,13 +1,12 @@
 // element-wise matrix multiplication and add
 
-#define BLOCK_SIZE 32
-
 __global__ void MatEleMul(float* A, float* B, float *C, int width, int height){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
     
     if(x < width && y < height){
-        C[y * width + x] = A[y * width + x] * B[y * width + x];
+        int index = y * width + x;
+        C[index] = A[index] * B[index];
     }
 }
 
@@ -16,6 +15,7 @@ __global__ void MatEleAdd(float* A, float* B, float *C, int width, int height){
     int y = threadIdx.y + blockDim.y * blockIdx.y;
     
     if(x < width && y < height){
-        C[y * width + x] = A[y * width + x] + B[y * width + x];
+        int index = y * width + x;
+        C[index] = A[index] + B[index];
     }
 }
