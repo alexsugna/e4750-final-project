c7a18ad224fe0d85aa98a6b9b9ed749476fc7778
skylers27 <32472386+skylers27@users.noreply.github.com>
2021-12-07 01:12:11 +0000

    Update ElementWise.cu
    switched width and height, got rid of C output in some functions

--- kernels/ElementWise.cu
+++ kernels/ElementWise.cu
@@ -1,6 +1,6 @@
 // element-wise matrix operations
 
-__global__ void MatEleMul(float* A, float* B, float *C, int width, int height){
+__global__ void MatEleMul(float* A, float* B, float *C, int height, int width){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
     
@@ -10,7 +10,7 @@ __global__ void MatEleMul(float* A, float* B, float *C, int width, int height){
     }
 }
 
-__global__ void MatEleAdd(float* A, float* B, float *C, int width, int height){
+__global__ void MatEleAdd(float* A, float* B, float *C, int height, int width){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
     
@@ -20,7 +20,7 @@ __global__ void MatEleAdd(float* A, float* B, float *C, int width, int height){
     }
 }
 
-__global__ void MatEleSubtract(float* A, float* B, float *C, int width, int height){
+__global__ void MatEleSubtract(float* A, float* B, float *C, int height, int width){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
     
@@ -30,7 +30,7 @@ __global__ void MatEleSubtract(float* A, float* B, float *C, int width, int heig
     }
 }
 
-__global__ void MatEleSquare(float* A, float *C, int width, int height){
+__global__ void MatEleSquare(float* A, float *C, int height, int width){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
     
@@ -40,12 +40,42 @@ __global__ void MatEleSquare(float* A, float *C, int width, int height){
     }
 }
 
-__global__ void MatEleDivide(float* A, float* B, float *C, int width, int height){
+__global__ void MatEleDivide(float* A, float* B, float *C, int height, int width){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
     
     if(x < width && y < height){
         int index = y * width + x;
         C[index] = A[index] / B[index];
+    }
+}
+
+__global__ void MatEleAdd2(float* A, float B, int height, int width){
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    
+    if(x < width && y < height){
+        int index = y * width + x;
+        A[index] = A[index] + B;
+    }
+}
+
+__global__ void MatEleMul2(float* A, float* B, int height, int width){
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    
+    if(x < width && y < height){
+        int index = y * width + x;
+        A[index] = A[index] * B[index];
+    }
+}
+
+__global__ void MatEleDivide2(float* A, float* B, int height, int width){
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    
+    if(x < width && y < height){
+        int index = y * width + x;
+        A[index] = A[index] / B[index];
     }
 }
