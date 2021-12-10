97ed6e5db78f9596d2db93351a6544d5aabedc5c
skylers27 <32472386+skylers27@users.noreply.github.com>
2021-12-10 07:50:12 +0000

    updated for cuda programs

--- kernels/ElementWise.cu
+++ kernels/ElementWise.cu
@@ -20,7 +20,7 @@ __global__ void MatEleAdd(float* A, float* B, float *C, int height, int width){
     }
 }
 
-__global__ void MatEleSubtract(float* A, float* B, float* C, int height, int width){
+__global__ void MatEleSubtract(float* A, float* B, float *C, int height, int width){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
     
@@ -77,5 +77,27 @@ __global__ void MatEleDivideInPlace(float* A, float* B, int height, int width){
     if(x < width && y < height){
         int index = y * width + x;
         A[index] = A[index] / B[index];
+    }
+}
+
+//divide all elements by the sum of the row
+__global__ void MatEleDivideRow(float* A, float* B, int height, int width){
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    if(x < width && y < height){
+        int index = y * width + x;
+        int row_idx = y;
+        A[index] = A[index] / B[row_idx];
+    }
+}
+
+// divide all elements by the sum of the column
+__global__ void MatEleDivideCol(float* A, float* B, int height, int width){
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    if(x < width && y < height){
+        int index = y * width + x;
+        int col_idx = x;
+        A[index] = A[index] / B[col_idx];
     }
 }
