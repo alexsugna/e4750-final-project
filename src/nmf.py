b2bc5e46550793267cf190d1ea934805b2a1a4ec
Alex Angus <ala2197@columbia.edu>
2021-12-12 20:30:40 +0000

    KL D progress

--- src/nmf.py
+++ src/nmf.py
@@ -26,7 +26,7 @@ def NMF_serial(X, W, H, iterations=100, loss='euclidean', eps=1e-16, return_time
 
         iterations=100 (int): the number of matrix factorization updates
 
-        loss='euclidean' (string): one of ['euclidean', 'divergence'] to specify the loss function
+        loss='euclidean' (string): one of ['euclidean', 'divergence'] to specify the loss function/update scheme
 
         eps=1e-16 (float): small value epsilon added for numerical stability
 
@@ -99,7 +99,7 @@ def NMF_parallel(X, W, H, iterations=100, loss='euclidean', eps=1e-16, return_ti
 
         iterations=100 (int): the number of matrix factorization updates
 
-        loss='euclidean' (string): one of ['euclidean', 'divergence'] to specify the loss function
+        loss='euclidean' (string): one of ['euclidean', 'divergence'] to specify the loss function/update scheme
 
         eps=1e-16 (float): small value epsilon added for numerical stability
 
@@ -118,7 +118,8 @@ def NMF_parallel(X, W, H, iterations=100, loss='euclidean', eps=1e-16, return_ti
     # define kernel paths
     kernel_paths = ['./kernels/MatrixMultiplication.cu',
                     './kernels/MatrixTranspose.cu',
-                    './kernels/ElementWise.cu']
+                    './kernels/ElementWise.cu',
+                    './kernels/RowColSum.cu']
 
     src_mod = context.getSourceModule(kernel_paths, multiple_kernels=True)
 
@@ -127,6 +128,11 @@ def NMF_parallel(X, W, H, iterations=100, loss='euclidean', eps=1e-16, return_ti
     func_tran = src_mod.get_function("MatTran") # matrix transpose
     func_add = src_mod.get_function("MatEleAddInPlace") # matrix elementwise addition
     func_div = src_mod.get_function("MatEleDivideInPlace") # matrix elementwise division
+    func_divC = self.mod.get_function("MatEleDivide") # gives output C matrix
+    func_row_sum = self.mod.get_function("row_sum") # matrix elementwise division
+    func_col_sum = self.mod.get_function("column_sum") # matrix elementwise division
+    func_row_div = self.mod.get_function("MatEleDivideRow") # divide row by sum
+    func_col_div = self.mod.get_function("MatEleDivideCol") # divide col by sum
 
     if return_time:
         # Event objects to mark start and end points
@@ -142,98 +148,237 @@ def NMF_parallel(X, W, H, iterations=100, loss='euclidean', eps=1e-16, return_ti
     X_d = gpuarray.to_gpu(X)
     W_d = gpuarray.to_gpu(W)
     H_d = gpuarray.to_gpu(H)
+    
+    if loss == 'euclidean':
+        """
+        Begin Euclidean Update
+        """
+        #define intermediate buffers on gpu for H update
+        Wt_d = gpuarray.zeros(((K, N)), dtype=np.float32)
+        WtX_d = gpuarray.zeros(((K, M)), dtype=np.float32)
+        WtW_d = gpuarray.zeros(((K, K)), dtype=np.float32)
+        WtWH_d = gpuarray.zeros(((K, M)), dtype=np.float32)
 
-    #define intermediate steps on gpu for H update
-    Wt_d = gpuarray.zeros(((K, N)), dtype=np.float32)
-    WtX_d = gpuarray.zeros(((K, M)), dtype=np.float32)
-    WtW_d = gpuarray.zeros(((K, K)), dtype=np.float32)
-    WtWH_d = gpuarray.zeros(((K, M)), dtype=np.float32)
+        # define itermediate buffers for W update
+        Ht_d = gpuarray.zeros(((M, K)), dtype=np.float32)
+        WH_d = gpuarray.zeros(((N, M)), dtype=np.float32)
+        WHHt_d = gpuarray.zeros(((N, K)), dtype=np.float32)
+        XHt_d = gpuarray.zeros(((N, K)), dtype=np.float32)
+        
+        # begin for loop for multiplicative updates
+        for i in range(1, iterations+1):
+            if i % 10 == 0 and print_iterations:
+                print('Iteration: %d' % i)
 
-    #itermediate steps for W update
-    Ht_d = gpuarray.zeros(((M, K)), dtype=np.float32)
-    WH_d = gpuarray.zeros(((N, M)), dtype=np.float32)
-    WHHt_d = gpuarray.zeros(((N, K)), dtype=np.float32)
-    XHt_d = gpuarray.zeros(((N, K)), dtype=np.float32)
+            """
+            Begin Euclidean Update H
+            """
+            # Wt = W.T
+            # H = H * Wt.dot(X) / (Wt.dot(W).dot(H) + eps)
 
-    # Record execution time and execute operation.
-    for i in range(1, iterations+1):
-        if i % 10 == 0 and print_iterations:
-            print('iteration %d' % i)
+            # Wt = W.T
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N, K)
+            event = func_tran(W_d, Wt_d, np.int32(K), np.int32(N), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #UPDATE H *****************************************************************************
-        #Wt = W.T
-        #H = H * Wt.dot(X) / (Wt.dot(W).dot(H) + err)
+            # Wt * X = WtX
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(K,M)
+            event = func_mul(Wt_d, X_d, WtX_d, np.int32(K), np.int32(N), np.int32(N), np.int32(M), np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
 
-        #Wt = W.T
-        block_dim, grid_dim = context.block_dims, context.grid_dims2d(N, K)
-        event = func_tran(W_d, Wt_d, np.int32(K), np.int32(N), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # Wt * W = WtW
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(K,K)
+            event = func_mul(Wt_d, W_d, WtW_d, np.int32(K), np.int32(N), np.int32(N), np.int32(K), np.int32(K), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #Wt * X = WtX
-        block_dim, grid_dim = context.block_dims, context.grid_dims2d(K,M)
-        event = func_mul(Wt_d, X_d, WtX_d, np.int32(K), np.int32(N), np.int32(N), np.int32(M), np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
+            # WtW * H = WtWH
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(K,M)
+            event = func_mul(WtW_d, H_d, WtWH_d, np.int32(K), np.int32(K), np.int32(K), np.int32(M), np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #Wt * W = WtW
-        block_dim, grid_dim = context.block_dims, context.grid_dims2d(K,K)
-        event = func_mul(Wt_d, W_d, WtW_d, np.int32(K), np.int32(N), np.int32(N), np.int32(K), np.int32(K), np.int32(K), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # WtWH + eps
+            event = func_add(WtWH_d, np.float32(eps), np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #WtW * H = WtWH
-        block_dim, grid_dim = context.block_dims, context.grid_dims2d(K,M)
-        event = func_mul(WtW_d, H_d, WtWH_d, np.int32(K), np.int32(K), np.int32(K), np.int32(M), np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # H .* WtX elementwise
+            event = func_ele_mul(H_d, WtX_d, np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #WtWH + err
-        event = func_add(WtWH_d, np.float32(eps), np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # H / WtWH elementwise
+            event = func_div(H_d, WtWH_d, np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+            """
+            End Euclidean Update H
+            """
+            """
+            Begin Euclidean Update W
+            """
+            # Ht = H.T #H transpose
+            event = func_tran(H_d, Ht_d, np.int32(M), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #H .* WtX elementwise
-        event = func_ele_mul(H_d, WtX_d, np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # X * Ht = XHt
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N,K)
+            event = func_mul(X_d, Ht_d, XHt_d, np.int32(N), np.int32(M), np.int32(M), np.int32(K), np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #H / WtWH elementwise
-        event = func_div(H_d, WtWH_d, np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # W * H = WH
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N,M)
+            event = func_mul(W_d, H_d, WH_d, np.int32(N), np.int32(K), np.int32(K), np.int32(M), np.int32(N), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        ##### W UPDATE #####
+            # WH * Ht = WHHt
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N,K)
+            event = func_mul(WH_d, Ht_d, WHHt_d, np.int32(N), np.int32(M), np.int32(M), np.int32(K), np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #Ht = H.T #H transpose
-        event = func_tran(H_d, Ht_d, np.int32(M), np.int32(K), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # WHHt + eps
+            event = func_add(WHHt_d, np.float32(eps), np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #X * Ht = XHt
-        block_dim, grid_dim = context.block_dims, context.grid_dims2d(N,K)
-        event = func_mul(X_d, Ht_d, XHt_d, np.int32(N), np.int32(M), np.int32(M), np.int32(K), np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # W .* XHt elementwise
+            event = func_ele_mul(W_d, XHt_d, np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #W * H = WH
-        block_dim, grid_dim = context.block_dims, context.grid_dims2d(N,M)
-        event = func_mul(W_d, H_d, WH_d, np.int32(N), np.int32(K), np.int32(K), np.int32(M), np.int32(N), np.int32(M), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # W / WHHt elementwise
+            event = func_div(W_d, WHHt_d, np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+            """
+            End Euclidean Update W
+            """
+        """
+        End Euclidean Update
+        """
+    
+    elif loss == 'divergence':
+        """
+        Begin Divergence Update
+        """
+        
+        # define intermediate buffers for H update
+        WH_d = gpuarray.zeros(((N,M)), dtype=np.float32)
+        P_d = gpuarray.zeros(((N,M)), dtype=np.float32)
+        Wt_d = gpuarray.zeros(((K,N)), dtype=np.float32)
+        Wt_sum_d = gpuarray.zeros(((K,1)), dtype=np.float32) #sum rows
+        WtP_d = gpuarray.zeros(((K,M)), dtype=np.float32)
 
-        #WH * Ht = WHHt
-        block_dim, grid_dim = context.block_dims, context.grid_dims2d(N,K)
-        event = func_mul(WH_d, Ht_d, WHHt_d, np.int32(N), np.int32(M), np.int32(M), np.int32(K), np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+        # define intermediate buffers on gpu for W update
+        Ht_d = gpuarray.zeros(((M,K)), dtype=np.float32)
+        Ht_sum_d = gpuarray.zeros(((1,K)), dtype=np.float32) #sum cols
+        PHt_d = gpuarray.zeros(((N,K)), dtype=np.float32)
+        
+        # begin for loop for multiplicative updates
+        for i in range(1, iterations+1):
+            if i % 10 == 0 and print_iterations:
+                print('Iteration: %d' % i)
+            
+            """
+            Begin Divergence Update H
+            """
+            # P = X / (W.dot(H)+eps) #intermediate step (purple matrix in notes)
+            # Wt = W.T
+            # Wt = Wt / Wt.sum(axis=1).reshape(-1, 1) #normalize rows
+            # H = H * (Wt.dot(P))  #update H
+            
+            # W.dot(H)
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N, M)
+            event = func_mul(W_d, H_d, WH_d, np.int32(N), np.int32(K), np.int32(K), np.int32(M), np.int32(N), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #WHHt + err
-        event = func_add(WHHt_d, np.float32(eps), np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # WH + eps
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N, M)
+            event = func_add(WH_d, np.float32(eps), np.int32(N), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #W .* XHt elementwise
-        event = func_ele_mul(W_d, XHt_d, np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # X / WH (saved as P_d)
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N, M)
+            event = func_divC(X_d, WH_d, P_d, np.int32(N), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #W / WHHt elementwise
-        event = func_div(W_d, WHHt_d, np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
-        cuda.Context.synchronize()
+            # Wt = W.T
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N, K)
+            event = func_tran(W_d, Wt_d, np.int32(K), np.int32(N), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-        #W = W * X.dot(Ht) / (W.dot(H).dot(Ht) + err) #update W
+            # Wt.sum(axis=1).reshape(-1, 1) #sum rows
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(K, 1)
+            event = func_row_sum(Wt_d, Wt_sum_d, np.int32(K), np.int32(N), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize() 
 
-#         euclidean_loss = euclidean_loss_parallel(X_d, WH_d, N, M, context, src_mod)
-#         euclidean_loss_np = euclidean_loss_numpy(X_d.get(), W_d.get(), H_d.get())
+            # Wt = Wt / Wt_sum_d #elementwise
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(K, N)
+            event = func_row_div(Wt_d, Wt_sum_d, np.int32(K), np.int32(N), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
 
-    #g_end.record()
+            # Wt.dot(P)
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(K, M)
+            event = func_mul(Wt_d, P_d, WtP_d, np.int32(K), np.int32(N), np.int32(N), np.int32(M), np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+            
+            # H .* WtP elementwise
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(K, M)
+            event = func_ele_mul(H_d, WtP_d, np.int32(K), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+            """
+            End Divergence Update H
+            """
+            """
+            Begin Divergence Update W
+            """
+            # P = X / (W.dot(H)+eps)
+            # Ht = H.T
+            # Ht = Ht / Ht.sum(axis=0).reshape(1, -1) #normalize columns
+            # W = W * (P.dot(Ht))  #update W
+            
+            # W.dot(H)
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N, M)
+            event = func_mul(W_d, H_d, WH_d, np.int32(N), np.int32(K), np.int32(K), np.int32(M), np.int32(N), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+            
+            # WH + eps
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N, M)
+            event = func_add(WH_d, np.float32(eps), np.int32(N), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+
+            # X / WH (saved as P_d)
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N, M)
+            event = func_divC(X_d, WH_d, P_d, np.int32(N), np.int32(M), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+
+            # Ht = H.T #H transpose
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(K, M)
+            event = func_tran(H_d, Ht_d, np.int32(M), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+
+            # Ht.sum(axis=0).reshape(1, -1) #sum columns
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(1, K)
+            event = func_col_sum(Ht_d, Ht_sum_d, np.int32(M), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+
+            # Ht = Ht / Ht_sum_d #elementwise
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(M, K)
+            event = func_col_div(Ht_d, Ht_sum_d, np.int32(M), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+
+            # P.dot(Ht)
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N, K)
+            event = func_mul(P_d, Ht_d, PHt_d, np.int32(N), np.int32(M), np.int32(M), np.int32(K), np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+
+            # W .* PHt elementwise
+            block_dim, grid_dim = context.block_dims, context.grid_dims2d(N, K)
+            event = func_ele_mul(W_d, PHt_d, np.int32(N), np.int32(K), block=block_dim, grid=grid_dim)
+            cuda.Context.synchronize()
+            """
+            End Divergence Update W
+            """
+        """
+        End Divergence Update
+        """
+        
+    else:
+        raise Exception('Loss function "{}" not supported.'.format(loss))
+        
+           
     # Fetch result from device to host
     H = H_d.get()
     W = W_d.get()
@@ -242,7 +387,7 @@ def NMF_parallel(X, W, H, iterations=100, loss='euclidean', eps=1e-16, return_ti
         g_end.record()
         g_end.synchronize()
 
-        total_time = g_start.time_till(g_end)*1e-3
+        total_time = g_start.time_till(g_end)*1e-3 # record total time in seconds
 
         return W, H, total_time
 
